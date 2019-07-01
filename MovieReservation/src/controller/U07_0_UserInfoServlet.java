10fb90c54a34365713b98959e1dc81a2dec2d52a
takunoumi <1701163@st.asojuku.ac.jp>
2019-07-01 12:03:04 +0900

    a

--- MovieReservation/src/controller/U07_0_UserInfoServlet.java
+++ MovieReservation/src/controller/U07_0_UserInfoServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/myPage")
+@WebServlet("/UserInfo")
 public class U07_0_UserInfoServlet extends HttpServlet {
 
 	@Override
