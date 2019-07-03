4cce98ec95859aed5090ed1396ae24235072b128
takunoumi <1701163@st.asojuku.ac.jp>
2019-07-03 10:06:46 +0900

    add moviereservation.jsp

--- MovieReservation/SQL/createTable.sql
+++ MovieReservation/SQL/createTable.sql
@@ -1,5 +1,13 @@
+-- データベース削除
+drop database MovieReservationSystem;
+
+-- データベース作成
+create database MovieReservationSystem;
+
+-- データベースの使用
 use MovieReservationSystem;
 
+-- 性別テーブルの作成
 create table gender(
 	`gender_code` VARCHAR(4) NOT NULL,
 	`gender` VARCHAR(128) NOT NULL,
@@ -10,6 +18,8 @@ INSERT INTO `gender`(`gender_code`,`gender`) VALUES ('F001','女性');
 INSERT INTO `gender`(`gender_code`,`gender`) VALUES ('M001','男性');
 INSERT INTO `gender`(`gender_code`,`gender`) VALUES ('A001','その他');
 
+
+-- ユーザーテーブルの作成
 create table user(
 	`user_code` VARCHAR(4) NOT NULL,
 	`user_mail` VARCHAR(256) NOT NULL,
@@ -24,6 +34,8 @@ create table user(
 
 INSERT INTO `user`(`user_code`,`user_mail`,`user_name`,`user_phone`,`gender_code`,`user_birth`,`user_pass`) VALUES('U001','1701163@st.asojuku.ac.jp','吉野拓海','090-9405-0043','M001','1998/04/03','aiueo'); /**/
 
+
+-- 料金テーブルの作成
 create table fee(
 	`fee_code` VARCHAR(4) NOT NULL,
 	`fee_type` VARCHAR(16) NOT NULL,
@@ -33,6 +45,8 @@ create table fee(
 
 INSERT INTO `fee`(`fee_code`,`fee_type`,`fee`) VALUES('FEE1','通常料金',3500);
 
+
+-- 映画テーブルの作成
 create table movie(
 	`movie_code` VARCHAR(16) NOT NULL,
 	`movie_name` VARCHAR(128) NOT NULL,
@@ -47,17 +61,30 @@ create table movie(
 
 INSERT INTO `movie`(`movie_code`,`movie_name`,`movie_time`,`movie_start`,`movie_finish`,`movie_adress`,`movie_thumbnail`,`movie_description`) VALUES('MOVIE01','名探偵ピカチュー',90,'2019/06/01','2019/08/01','https://meitantei-pikachu.jp/','../thumbnail/pikachu','全世界待望のハリウッド感動（電）超大作');
 
+-- シアターテーブルの作成
+create table theater(
+	`theater_code` VARCHAR(128) NOT NULL,
+	`max_seat_space` INTEGER NOT NULL,
+	PRIMARY KEY(`theater_code`)
+);
+
+INSERT INTO `theater`(`theater_code`,`max_seat_space`) VALUES('THEATER01',100);
+
+-- 上映テーブルの作成
 create table shows(
 	`show_code` VARCHAR(32) NOT NULL,
 	`movie_code` VARCHAR(16) NOT NULL,
 	`show_date` DATETIME NOT NULL,
 	`theater_code` VARCHAR(128) NOT NULL,
+	`seat_space` INTEGER NOT NULL,
 	PRIMARY KEY(`show_code`),
-	FOREIGN KEY `shows`(`movie_code`) REFERENCES `movie`(`movie_code`)
+	FOREIGN KEY (`movie_code`) REFERENCES `movie`(`movie_code`),
+	FOREIGN KEY (`theater_code`) REFERENCES `theater`(`theater_code`)
 );
 
-INSERT INTO `shows`(`show_code`,`movie_code`,`show_date`,`theater_code`) VALUES('S001','MOVIE01','2019/06/01 00:00:00','THEATER01');
+INSERT INTO `shows`(`show_code`,`movie_code`,`show_date`,`theater_code`,`seat_space`) VALUES('S001','MOVIE01','2019/06/01 00:00:00','THEATER01',100);
 
+-- 予約テーブルの作成
 create table reservation(
 	`reservation_code` VARCHAR(32) NOT NULL,
 	`user_code` VARCHAR(4) NOT NULL,
@@ -69,6 +96,7 @@ create table reservation(
 
 INSERT INTO `reservation`(`reservation_code`,`user_code`,`show_code`) VALUES('RESERVATION01','U001','S001');/**/
 
+-- 予約詳細のテーブル作成
 create table reservation_details(
 	`reservation_code` VARCHAR(32) NOT NULL,
 	`seat_code` VARCHAR(4) NOT NULL,
@@ -76,11 +104,12 @@ create table reservation_details(
 	PRIMARY KEY(`reservation_code`),
 	FOREIGN KEY (`reservation_code`) REFERENCES `reservation`(`reservation_code`),
 	FOREIGN KEY (`fee_code`) REFERENCES `fee`(`fee_code`)
-);/**/
+);
 
 INSERT INTO `reservation_details`(`reservation_code`,`seat_code`,`fee_code`) VALUES('RESERVATION01','A10','FEE1');
 
 
+-- セレクト文
 select * from gender;
 select * from user;
 select * from fee;
--- MovieReservation/src/controller/U18_MovieReservationServlet.java
+++ MovieReservation/src/controller/U18_MovieReservationServlet.java
@@ -14,6 +14,9 @@ public class U18_MovieReservationServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/top.jsp");
 		dispatcher.forward(request, response);
 	}
