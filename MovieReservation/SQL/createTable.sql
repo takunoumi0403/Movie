fe7d02ecced80c5d0d30eef0be8c7e9b776a9336
takunoumi <1701163@st.asojuku.ac.jp>
2019-07-15 09:41:29 +0900

    Update Sql

--- MovieReservation/SQL/createTable.sql
+++ MovieReservation/SQL/createTable.sql
@@ -7,21 +7,32 @@ create database MovieReservationSystem;
 -- データベースの使用
 use MovieReservationSystem;
 
+-- シアターテーブルの作成
+create table theater(
+	`theater_code` INTEGER AUTO_INCREMENT NOT NULL,
+	`max_seat_space` INTEGER NOT NULL,
+	PRIMARY KEY(`theater_code`)
+);
+
+INSERT INTO `theater`(`max_seat_space`) VALUES (325);
+INSERT INTO `theater`(`max_seat_space`) VALUES (162);
+INSERT INTO `theater`(`max_seat_space`) VALUES (224);
+
 -- 性別テーブルの作成
 create table gender(
-	`gender_code` VARCHAR(4) NOT NULL,
-	`gender` VARCHAR(128) NOT NULL,
+	`gender_code` VARCHAR(1) NOT NULL,
+	`gender` VARCHAR(32) NOT NULL,
 	PRIMARY KEY(`gender_code`)
 );
 
-INSERT INTO `gender`(`gender_code`,`gender`) VALUES ('F001','女性');
-INSERT INTO `gender`(`gender_code`,`gender`) VALUES ('M001','男性');
-INSERT INTO `gender`(`gender_code`,`gender`) VALUES ('A001','その他');
+INSERT INTO `gender`(`gender_code`,`gender`) VALUES ('F','女性');
+INSERT INTO `gender`(`gender_code`,`gender`) VALUES ('M','男性');
+INSERT INTO `gender`(`gender_code`,`gender`) VALUES ('A','その他');
 
 
 -- ユーザーテーブルの作成
 create table user(
-	`user_code` VARCHAR(4) NOT NULL,
+	`user_code` INTEGER AUTO_INCREMENT NOT NULL,
 	`user_mail` VARCHAR(256) NOT NULL,
 	`user_name` VARCHAR(128) NOT NULL,
 	`user_phone` VARCHAR(16) NOT NULL,
@@ -32,23 +43,24 @@ create table user(
 	FOREIGN KEY `user`(gender_code) REFERENCES `gender`(gender_code)
 );
 
-INSERT INTO `user`(`user_code`,`user_mail`,`user_name`,`user_phone`,`gender_code`,`user_birth`,`user_pass`) VALUES('U001','1701163@st.asojuku.ac.jp','吉野拓海','090-9405-0043','M001','1998/04/03','aiueo'); /**/
+INSERT INTO `user`(`user_mail`,`user_name`,`user_phone`,`gender_code`,`user_birth`,`user_pass`) VALUES('1701163@st.asojuku.ac.jp','吉野拓海','090-9405-0043','M','1998/04/03','aiueo');
 
 
 -- 料金テーブルの作成
 create table fee(
-	`fee_code` VARCHAR(4) NOT NULL,
-	`fee_type` VARCHAR(16) NOT NULL,
+	`fee_code` INTEGER AUTO_INCREMENT NOT NULL,
+	`fee_type` VARCHAR(128) NOT NULL,
 	`fee` INTEGER NOT NULL,
 	PRIMARY KEY(`fee_code`)
 );
 
-INSERT INTO `fee`(`fee_code`,`fee_type`,`fee`) VALUES('FEE1','通常料金',3500);
+INSERT INTO `fee`(`fee_type`,`fee`) VALUES('通常料金',3500);
+INSERT INTO `fee`(`fee_type`,`fee`) VALUES('半額',1750);
 
 
 -- 映画テーブルの作成
 create table movie(
-	`movie_code` VARCHAR(16) NOT NULL,
+	`movie_code` INTEGER AUTO_INCREMENT NOT NULL,
 	`movie_name` VARCHAR(128) NOT NULL,
 	`movie_time` INTEGER NOT NULL,
 	`movie_start` DATE NOT NULL,
@@ -59,54 +71,46 @@ create table movie(
 	PRIMARY KEY(`movie_code`)
 );
 
-INSERT INTO `movie`(`movie_code`,`movie_name`,`movie_time`,`movie_start`,`movie_finish`,`movie_adress`,`movie_thumbnail`,`movie_description`) VALUES('MOVIE01','名探偵ピカチュー',90,'2019/06/01','2019/08/01','https://meitantei-pikachu.jp/','../thumbnail/pikachu','全世界待望のハリウッド感動（電）超大作');
-
--- シアターテーブルの作成
-create table theater(
-	`theater_code` VARCHAR(128) NOT NULL,
-	`max_seat_space` INTEGER NOT NULL,
-	PRIMARY KEY(`theater_code`)
-);
-
-INSERT INTO `theater`(`theater_code`,`max_seat_space`) VALUES('THEATER01',100);
+INSERT INTO `movie`(`movie_name`,`movie_time`,`movie_start`,`movie_finish`,`movie_adress`,`movie_thumbnail`,`movie_description`) VALUES('名探偵ピカチュー',90,'2019/06/01','2019/08/01','https://meitantei-pikachu.jp/','../thumbnail/pikachu','全世界待望のハリウッド感動（電）超大作');
 
 -- 上映テーブルの作成
 create table shows(
-	`show_code` VARCHAR(32) NOT NULL,
-	`movie_code` VARCHAR(16) NOT NULL,
+	`show_code` INTEGER AUTO_INCREMENT NOT NULL,
+	`movie_code` INTEGER NOT NULL,
 	`show_date` DATETIME NOT NULL,
-	`theater_code` VARCHAR(128) NOT NULL,
+	`theater_code` INTEGER NOT NULL,
 	`seat_space` INTEGER NOT NULL,
 	PRIMARY KEY(`show_code`),
 	FOREIGN KEY (`movie_code`) REFERENCES `movie`(`movie_code`),
 	FOREIGN KEY (`theater_code`) REFERENCES `theater`(`theater_code`)
 );
 
-INSERT INTO `shows`(`show_code`,`movie_code`,`show_date`,`theater_code`,`seat_space`) VALUES('S001','MOVIE01','2019/06/01 00:00:00','THEATER01',100);
+INSERT INTO `shows`(`show_code`,`movie_code`,`show_date`,`theater_code`,`seat_space`) VALUES(1,1,'2019/06/01 00:00:00',1,325);
 
 -- 予約テーブルの作成
 create table reservation(
-	`reservation_code` VARCHAR(32) NOT NULL,
-	`user_code` VARCHAR(4) NOT NULL,
-	`show_code` VARCHAR(32) NOT NULL,
+	`reservation_code` INTEGER AUTO_INCREMENT NOT NULL,
+	`user_code` INTEGER NOT NULL,
+	`show_code` INTEGER NOT NULL,
 	PRIMARY KEY(`reservation_code`),
 	FOREIGN KEY (`user_code`) REFERENCES `user`(`user_code`),
 	FOREIGN KEY (`show_code`) REFERENCES `shows`(`show_code`)
 );
 
-INSERT INTO `reservation`(`reservation_code`,`user_code`,`show_code`) VALUES('RESERVATION01','U001','S001');/**/
+INSERT INTO `reservation`(`user_code`,`show_code`) VALUES(1,1);
 
 -- 予約詳細のテーブル作成
 create table reservation_details(
-	`reservation_code` VARCHAR(32) NOT NULL,
-	`seat_code` VARCHAR(4) NOT NULL,
-	`fee_code` VARCHAR(4) NOT NULL,
-	PRIMARY KEY(`reservation_code`),
+	`reservation_code` INTEGER NOT NULL,
+	`detail_number` INTEGER NOT NULL,
+	`seat_number` INTEGER NOT NULL,
+	`fee_code` INTEGER NOT NULL,
+	PRIMARY KEY(reservation_code,detail_number),
 	FOREIGN KEY (`reservation_code`) REFERENCES `reservation`(`reservation_code`),
 	FOREIGN KEY (`fee_code`) REFERENCES `fee`(`fee_code`)
 );
 
-INSERT INTO `reservation_details`(`reservation_code`,`seat_code`,`fee_code`) VALUES('RESERVATION01','A10','FEE1');
+INSERT INTO `reservation_details`(`reservation_code`,`detail_number`,`seat_number`,`fee_code`) VALUES(1,1,5,1);
 
 
 -- セレクト文
