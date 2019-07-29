da8e6bae04aab4f9acaa23efb2cf015e6118ad87
1701166 <1701166@st.asojuku.ac.jp>
2019-07-29 11:22:05 +0900

    u16更新

--- MovieReservation/src/model/UserDeleteModel.java
+++ MovieReservation/src/model/UserDeleteModel.java
@@ -2,18 +2,16 @@ package model;
 
 import java.sql.SQLException;
 
-import beans.UserInfoBeans;
 import dao.UserDeleteDao;
 
 public class UserDeleteModel {
 
-	public void deleteUser() {
-
-		UserInfoBeans beans = new UserInfoBeans();
+	public void deleteUser(String userCode) throws Exception {
 
 		UserDeleteDao dao = new UserDeleteDao();
 		try {
-			dao.userDelete(beans.getUserCode());
+			dao.connect();
+			dao.userDelete(userCode);
 		} catch (SQLException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
